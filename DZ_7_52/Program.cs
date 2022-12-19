c15543d8695ce8661483fc60c7cd1567bb1bb252
kknorth <kirillacid@gmail.com>
2022-12-19 12:50:49 +0700

    Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.

---
+++ DZ_7_52/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//  Например, задан массив:
+//  1 4 7 2
+//  5 9 2 3
+//  8 4 2 4
+//  Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+void FillArray(int[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] array)   
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void SearchAver(int[,] array, int x, int y)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        double average = sum / x;
+        double n = j + 1;
+    Console.WriteLine($"Столбец {n}: Среднеарефметическое = {Math.Round(average,1)}");
+    } 
+}
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int y = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[x, y];
+FillArray(array);
+PrintArray(array);
+SearchAver(array, x, y);
