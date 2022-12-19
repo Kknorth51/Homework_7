dab16bc5740d83904664edd312413e2e8a427d9e
kknorth <kirillacid@gmail.com>
2022-12-19 13:54:13 +0700

    Zadacha 50

--- DZ_7_50/Program.cs
+++ DZ_7_50/Program.cs
@@ -6,42 +6,6 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-// Console.WriteLine("Введите резмер массива");
-// int m = Convert.ToInt32(Console.ReadLine());
-// int n = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите искомое число");
-// int s = Convert.ToInt32(Console.ReadLine());
-// int[,] array = new int[m, n];
-// int count = 0;
-
-// void FillArray(int[,] arr)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(1, 10);
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-// FillArray(array);
-
-// for (int e = 0; e < array.GetLength(0); e++)
-// {
-//     for (int r = 0; r < array.GetLength(1); r++)
-//     {
-//         if (array[e, r] == s)
-//         {
-//             Console.WriteLine($"Искомое число находится в координате {e} и {r}");
-//             count++;
-//         }
-//     }
-//     Console.WriteLine();
-// }
-// if (count <= 0) Console.WriteLine($"Искомого числа нет!");
-
 Console.WriteLine("Введите номер строки и столбца искомого элемента:");
 int m = Convert.ToInt32(Console.ReadLine());
 int n = Convert.ToInt32(Console.ReadLine());
